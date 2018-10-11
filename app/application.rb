8e17f7f77c17cb54b03f800a9272e050ab8b9976
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-10-11 23:11:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.new
+    time = Time.new(2012,11,11,9,30)
 
     if time.hour > 12
       resp.write "Good Afternoon"
